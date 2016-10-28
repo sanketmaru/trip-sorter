3d0a23370a48befb7ff7d64a44ba56b48dacb0fe
sanketmaru <sanketmaru@gmail.com>
2016-10-28 20:14:37 +0530

    logic added for cheapest and fastest deals

--- components/home/home-controller.js
+++ components/home/home-controller.js
@@ -4,32 +4,14 @@ angular.module('TripSorter')
 
     var init = init;
     var initCities = initCities;
-
+    var ctrl = this;
     $scope.routesType = {
       'all' : 'all',
       'cheapest' : 'cheapest',
       'fastest' : 'fastest' 
     };
 
-    $scope.selectedRoute = $scope.routesType.cheapest; 
-
-    function init() {
-      RequestService.get('response.json')
-        .then(function(response){
-
-          var deals = response.data.deals;
-          $scope.arrivals = _.uniq(_.pluck(deals, 'arrival'));
-          $scope.departures = _.uniq(_.pluck(deals, 'departure'));
-
-        }).catch(function(error){
-          alert(error); // toaster can be used
-        });
-    };
-
-    function initCities(){
-      $scope.depCity = 'Departure City';
-      $scope.arrCity = 'Arrival City';
-    };
+    $scope.selectedRoute = $scope.routesType.cheapest;  
 
     initCities();
     init();
@@ -52,6 +34,81 @@ angular.module('TripSorter')
       initCities();
     };
 
+    $scope.search = function() {
+      
+      $scope.currentDeals = getCurrentDeals();
+      
+    };
+
+    function init() {
+      RequestService.get('response.json')
+        .then(function(response){
+
+          ctrl.deals = response.data.deals;
+          $scope.arrivals = _.uniq(_.pluck(ctrl.deals, 'arrival'));
+          $scope.departures = _.uniq(_.pluck(ctrl.deals, 'departure'));
+
+        }).catch(function(error){
+          alert(error); // toaster can be used
+        });
+    };
+
+    function initCities(){
+      $scope.depCity = 'Departure City';
+      $scope.arrCity = 'Arrival City';
+    };
+
+    function getCurrentDeals() {
+      
+      var deals = [];
+
+      if(!$scope.depCity) {
+        alert('Please select departure city'); //TODO toaster can be used to make look better
+        return;
+      }
+
+      if(!$scope.arrCity) {
+        alert('Please select arrival city'); //TODO toaster can be used to make look better
+        return;
+      }
+
+      var currentDeals = ctrl.deals.filter(function(deal){
+        return deal.departure === $scope.depCity && deal.arrival === $scope.arrCity ; 
+      });
+
+      if(!currentDeals.length) {
+        return;
+      }
+
+      if($scope.selectedRoute === $scope.routesType.cheapest) {
+        var cheapestDeal = currentDeals[0];
+        var initialFare = cheapestDeal.cost - cheapestDeal.discount;
+        currentDeals.forEach(function(currentDeal){
+          var fare = currentDeal.cost - currentDeal.discount;
+          if(fare <  initialFare) {
+            cheapestDeal = currentDeal;
+          }
+        });
+        deals.push(cheapestDeal);
+      }
+
+
+      if($scope.selectedRoute === $scope.routesType.fastest) {
+        var fastestDeal = currentDeals[0]; 
+        var initialDurationInMin = ( fastestDeal.duration.h * 60 ) + (fastestDeal.duration.m);
+        currentDeals.forEach(function(currentDeal){
+          var durationInMin = ( currentDeal.duration.h * 60 ) + (currentDeal.duration.m);
+          if(durationInMin <  initialDurationInMin) {
+            fastestDeal = currentDeal;
+          }
+        });
+        deals.push(fastestDeal);
+      }
+
+      deals = currentDeals;
+
+      return deals;
+    }
 
 
   }]);
