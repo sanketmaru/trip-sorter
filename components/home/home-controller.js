47c2d36363f990aa98bdd4989859e3deab9ec551
sanketmaru <sanketmaru@gmail.com>
2016-10-28 16:33:29 +0530

    reset and more html changes

--- components/home/home-controller.js
+++ components/home/home-controller.js
@@ -3,7 +3,7 @@ angular.module('TripSorter')
 
 
     var init = init;
-
+    var initCities = initCities;
 
     $scope.routesType = {
       'all' : 'all',
@@ -18,20 +18,39 @@ angular.module('TripSorter')
         .then(function(response){
 
           var deals = response.data.deals;
-
-
+          $scope.arrivals = _.uniq(_.pluck(deals, 'arrival'));
+          $scope.departures = _.uniq(_.pluck(deals, 'departure'));
 
         }).catch(function(error){
           alert(error); // toaster can be used
         });
     };
 
+    function initCities(){
+      $scope.depCity = 'Departure City';
+      $scope.arrCity = 'Arrival City';
+    };
+
+    initCities();
     init();
 
     $scope.routeChanged = function() {
       console.log('current route is ', $scope.selectedRoute);
+      console.log('dep ', $scope.depCity);
+      console.log('arr ', $scope.arrCity);
     };
 
+    $scope.changeDepartureCity = function(city) {
+      $scope.depCity = city;
+    };
+
+    $scope.changeArrivalCity = function(city) {
+      $scope.arrCity = city;
+    };
+
+    $scope.reset = function() {
+      initCities();
+    };
 
 
 
